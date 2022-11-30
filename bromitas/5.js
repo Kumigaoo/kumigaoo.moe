57c9ab1a33df3c8e99bde5d8a1ea6b5f89859da5
kumigaoo <kumigaoo@gmail.com>
2022-11-30 20:28:45 +0100

    Rimas hasta el 9

--- bromitas/5.js
+++ bromitas/5.js
@@ -5,7 +5,7 @@ document.addEventListener("keydown", (event) => {
     setTimeout(() => {
       document.getElementsByClassName("culo")[0].style.display = "none";
     }, 5e3);
-  } else if (prevkey == "1" && event.key == "3") {
+  } else if (prevkey == "1" && setTimeout(3e3) && event.key == "3") {
     document.getElementsByClassName("agarrame")[0].style.display = "flex";
     setTimeout(() => {
       document.getElementsByClassName("agarrame")[0].style.display = "none";
@@ -24,6 +24,26 @@ document.addEventListener("keydown", (event) => {
     document.getElementsByClassName("aparato")[0].style.display = "flex";
     setTimeout(() => {
       document.getElementsByClassName("aparato")[0].style.display = "none";
+    }, 5e3);
+  } else if (event.key == "6") {
+    document.getElementsByClassName("veis")[0].style.display = "flex";
+    setTimeout(() => {
+      document.getElementsByClassName("veis")[0].style.display = "none";
+    }, 5e3);
+  } else if (event.key == "7") {
+    document.getElementsByClassName("mete")[0].style.display = "flex";
+    setTimeout(() => {
+      document.getElementsByClassName("mete")[0].style.display = "none";
+    }, 5e3);
+  } else if (event.key == "8") {
+    document.getElementsByClassName("entocho")[0].style.display = "flex";
+    setTimeout(() => {
+      document.getElementsByClassName("entocho")[0].style.display = "none";
+    }, 5e3);
+  } else if (event.key == "9") {
+    document.getElementsByClassName("mueve")[0].style.display = "flex";
+    setTimeout(() => {
+      document.getElementsByClassName("mueve")[0].style.display = "none";
     }, 5e3);
   }
   prevkey = event.key;
