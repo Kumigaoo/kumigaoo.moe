c62081b124cd4a8623a52cde3da7f42c73fb6e5d
kumigaoo <kumigaoo@gmail.com>
2022-11-30 23:06:09 +0100

    acortamiento javascript

--- bromitas/5.js
+++ bromitas/5.js
@@ -1,50 +1,33 @@
-let prevkey = " ";
+let frases = {
+  1: "Por el culo te vacuno",
+  3: "Te la meto del reves",
+  4: "Por tu culo mi aparato",
+  5: "Por el culo te la hinco",
+  6: "Si os agachais me la veis",
+  7: "Por el culo se te mete",
+  8: "Por el culo te la entocho",
+  9: "Agarrame la que se me mueve",
+  13: "Agarrame la que me crece",
+};
+
+let keys = "";
+let showingmessage = false;
 document.addEventListener("keydown", (event) => {
-  if (event.key == "5") {
-    document.getElementsByClassName("culo")[0].style.display = "flex";
+  if (showingmessage) return;
+  keys += event.key;
+  let options = Object.keys(frases).filter((i) => i.startsWith(keys));
+  let timeout = 0.5e3;
+  if (options.length == 0) return;
+  if (options.length == 1) timeout = 1;
+  setTimeout(() => {
+    if (!frases.hasOwnProperty(keys) || showingmessage) return;
+    document.getElementById("rimas").innerHTML = frases[keys];
+    document.getElementById("rimas").style.display = "flex";
+    showingmessage = true;
     setTimeout(() => {
-      document.getElementsByClassName("culo")[0].style.display = "none";
-    }, 5e3);
-  } else if (prevkey == "1" && setTimeout(3e3) && event.key == "3") {
-    document.getElementsByClassName("agarrame")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("agarrame")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "1") {
-    document.getElementsByClassName("vacuno")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("vacuno")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "3") {
-    document.getElementsByClassName("reves")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("reves")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "4") {
-    document.getElementsByClassName("aparato")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("aparato")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "6") {
-    document.getElementsByClassName("veis")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("veis")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "7") {
-    document.getElementsByClassName("mete")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("mete")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "8") {
-    document.getElementsByClassName("entocho")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("entocho")[0].style.display = "none";
-    }, 5e3);
-  } else if (event.key == "9") {
-    document.getElementsByClassName("mueve")[0].style.display = "flex";
-    setTimeout(() => {
-      document.getElementsByClassName("mueve")[0].style.display = "none";
-    }, 5e3);
-  }
-  prevkey = event.key;
+      document.getElementById("rimas").style.display = "none";
+      showingmessage = false;
+    }, 4e3);
+    keys = "";
+  }, timeout);
 });
