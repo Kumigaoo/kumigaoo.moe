c27adcb366dd285479da4a459e563546b659c57d
KumiGaoo <kumigaoo@gmail.com>
2023-09-14 11:28:02 +0200

    sigue sin ir lmao

--- TeaType/tearandom.js
+++ TeaType/tearandom.js
@@ -1,19 +1,22 @@
-function shuffle(a){
-c = a.length;
-while(c) b = Math.random() * (--c + 1) | 0, d = a[c], a[c] = a[b], a[b] = d;
-return a
+function shuffle(a) {
+  c = a.length;
+  while (c)
+    (b = (Math.random() * (--c + 1)) | 0),
+      (d = a[c]),
+      (a[c] = a[b]),
+      (a[b] = d);
+  return a;
 }
 
-function choosing(){
-    let vars ={
-       Tieguanyin: ["Tieguanyin AAAAA"],
-       Osmanthus: ["Osmanthus Tieguanyin AAA"],
-       LRP: ["Lu Rou Pian"]
-    }
-
-let array = [];
-    document.querySelectorAll ('[type=checkbox]:checked').forEach(el => {
-        array = [...array, vars[el.value][Math.floor(Math.random() * vars[el.value].length)]]
-    });
-document.getElementById("Té").innerHTML = shuffle(array). join(" ");
+function choosing() {
+  let names = {
+    Tieguanyin: "Tieguanyin AAAAA",
+    Osmanthus: "Osmanthus Tieguanyin AAA",
+    LRP: "Lu Rou Pian",
+  };
+  let array = [];
+  document.querySelectorAll("[type=checkbox]:checked").forEach((el) => {
+    array.push(el.names);
+    document.getElementById("Té").innerHTML = names[shuffle(array)[0]];
+  });
 }
