c0967c17109e316d690db0db1aa9f3e3add88184
KumiGaoo <kumigaoo@gmail.com>
2023-09-14 09:58:44 +0200

    random choosing (no funca)

--- TeaType/tearandom.js
+++ TeaType/tearandom.js
@@ -1,5 +1,19 @@
-var tea[
-    
-  ];
-  var randomNumber = Math.floor(Math.random() * textArray.length);
-  document.getElementById("randomNumber").innerHTML = textArray[randomNumber];
+function shuffle(a){
+c = a.length;
+while(c) b = Math.random() * (--c + 1) | 0, d = a[c], a[c] = a[b], a[b] = d;
+return a
+}
+
+function choosing(){
+    let vars ={
+       Tieguanyin: ["Tieguanyin AAAAA"],
+       Osmanthus: ["Osmanthus Tieguanyin AAA"],
+       LRP: ["Lu Rou Pian"]
+    }
+
+let array = [];
+    document.querySelectorAll ('[type=checkbox]:checked').forEach(el => {
+        array = [...array, vars[el.value][Math.floor(Math.random() * vars[el.value].length)]]
+    });
+document.getElementById("Té").innerHTML = shuffle(array). join(" ");
+}
