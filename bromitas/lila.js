62bf9cccfa61329e35e8203d135c376605255c64
Kumigaoo <kumigaoo@gmail.com>
2024-04-15 20:32:58 +0200

    kumi2 and lila linkedin and others

--- bromitas/lila.js
+++ bromitas/lila.js
@@ -14,5 +14,35 @@ function movimiento() {
     caja.style.top = ranval(-10, 90) + "%";
     caja.style.left = ranval(-10, 90) + "%";
     caja.style.fontSize = ranval(5, 20) + "vw";
+    if (Math.random() >= 0.5) {
+      caja.style.color = "black";
+    } else {
+      caja.style.color = "white";
+    }
   });
 }
+
+let frases = {
+  kumi: () => (window.location.href = "/kumi.html"),
+  lila: () => (window.location.href = "/home.html"),
+};
+
+let keys = "";
+let showingmessage = false;
+document.addEventListener("keydown", (event) => {
+  if (showingmessage) return;
+  keys += event.key;
+  let options = Object.keys(frases).filter((i) => i.startsWith(keys));
+  let timeout = 0.5e3;
+  if (options.length == 0) return;
+  if (options.length == 1) timeout = 1;
+  setTimeout(() => {
+    if (!frases.hasOwnProperty(keys) || showingmessage) return;
+    if (typeof frases[keys] == "function") {
+      frases[keys]();
+      keys = "";
+      return;
+    }
+    keys = "";
+  }, timeout);
+});
