9dfd38ca0b9184b7c6bd4a9bed04d84673c97545
KumiGaoo <119248514+Kumigaoo@users.noreply.github.com>
2025-11-25 10:27:10 +0100

    21 date

--- bromitas/5.js
+++ bromitas/5.js
@@ -9,7 +9,7 @@ let frases = {
   9: "Agárrame la que se me mueve",
   13: "Agárrame la que me crece",
   18: "Por el culo te la entocho",
-  20: () => (window.location.href = "/contador"),
+  21: () => (window.location.href = "/contador"),
   lila: () => (window.location.href = "/lila"),
 };
 
